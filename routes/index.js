bbdaa53f177d63bb23f236de0a2bc9c2a24a61d1
Jairo Iglesias (SPARK) <jairo.iglesias@sparkconsultoria.com>
2019-08-15 03:50:40 -0300

    edp return status machines

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,69 @@ var express = require('express');
 var router = express.Router();
 const superagent = require('superagent');
 
+let statusMachines = [
+  {
+    id: 1,
+    machineName: 'Modelo J',
+    machineStatus: [
+      {name: 'valvula X', valor: 15},
+      {name: 'valvula Y', valor: 30},
+      {name: 'motor A', valor: 40}
+    ],
+    statusTotal: 85
+  },
+  {
+    id: 2,
+    machineName: 'Modelo L',
+    machineStatus: [
+      {name: 'valvula X', valor: 20},
+      {name: 'valvula Y', valor: 40},
+      {name: 'motor A', valor: 20}
+    ],
+    statusTotal: 80
+  },
+  {
+    id: 3,
+    machineName: 'Modelo M',
+    machineStatus: [
+      {name: 'valvula X', valor: 13},
+      {name: 'valvula Y', valor: 56},
+      {name: 'motor A', valor: 16}
+    ],
+    statusTotal: 85
+  },
+  {
+    id: 4,
+    machineName: 'Modelo N',
+    machineStatus: [
+      {name: 'valvula X', valor: 15},
+      {name: 'valvula Y', valor: 45},
+      {name: 'motor A', valor: 22}
+    ],
+    statusTotal: 72
+  },
+  {
+    id: 5,
+    machineName: 'Modelo O',
+    machineStatus: [
+      {name: 'valvula X', valor: 20},
+      {name: 'valvula Y', valor: 10},
+      {name: 'motor A', valor: 60}
+    ],
+    statusTotal: 90
+  },
+  {
+    id: 6,
+    machineName: 'Modelo P',
+    machineStatus: [
+      {name: 'valvula X', valor: 35},
+      {name: 'valvula Y', valor: 31},
+      {name: 'motor A', valor: 27}
+    ],
+    statusTotal: 93
+  }
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -15,7 +78,8 @@ router.get('/superagent', (req, res) => {
 
   superagent.get('https://hacka-siemens-api-middleware.herokuapp.com/appapi/test').then(response => {
 
-    res.send("123 "+ response.text)
+    // res.send("123 "+ response.text)
+    res.status(200).json(statusMachines)
 
   })
 })
